b35b465e473c857c0e335076e6f1351ec95d20bb
SahaHungkul <mhasbysyabani27@gmail.com>
2026-04-02 18:39:20 +0700

    konfigurasi migration

---
+++ peminjaman-alat/database/migrations/2026_04_02_112911_create_loans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('tool_id')->constrained('tools');
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali_rencana');
+            $table->date('tanggal_kembali_aktual');
+            $table->enum('status',['pending','disetujui','ditolak','kembali'])->default('pending');
+            $table->foreignId('petugas_id')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
